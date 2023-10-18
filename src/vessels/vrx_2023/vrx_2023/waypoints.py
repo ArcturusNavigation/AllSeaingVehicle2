71e1f6b3d268fae9161152e7d639be9626f0b942
Theo Gerst <teogerst@gmail.com>
2023-10-17 20:24:19 -0400

    node to receive waypoints and send to gateway

---
+++ src/vessels/vrx_2023/vrx_2023/waypoints.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import PoseArray
+from std_msgs.msg import String
+
+class WaypointPubSub(Node):
+
+    def __init__(self):
+        super().__init__('waypoint_pub_sub')
+        self.subscription = self.create_subscription(
+            PoseArray,
+            '/vrx/wayfinding/waypoints',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.publisher_ = self.create_publisher(String, '/gateway_msgs', 10)
+
+
+    def listener_callback(self, msg):
+        new_msg = String()
+        inner_string = ""
+        for i, pose in enumerate(msg.poses):
+            inner_string += f"{pose.position.y},{pose.position.x}"
+            if i < len(msg.poses) - 1:
+                inner_string += ":"
+        new_msg.data = inner_string
+        self.publisher_.publish(new_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    pub_sub = WaypointPubSub()
+
+    rclpy.spin(pub_sub)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    pub_sub.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
