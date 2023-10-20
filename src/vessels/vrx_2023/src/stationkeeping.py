e2f76341d9b966a9adf1025ed1602a7d7103f67d
Toya Takahashi <toyatakahashi522@gmail.com>
2023-10-20 10:42:22 -0400

    generalized wayfinding to stationkeeping

--- src/vessels/vrx_2023/src/stationkeeping.py
+++ src/vessels/vrx_2023/src/stationkeeping.py
@@ -2,37 +2,32 @@
 import rclpy
 from rclpy.node import Node
 
-from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import PoseStamped
 from protobuf_client_interfaces.msg import Gateway
 
-class WaypointPubSub(Node):
+class StationKeeping(Node):
 
     def __init__(self):
-        super().__init__('waypoint_pub_sub')
+        super().__init__('stationkeeping')
         self.subscription = self.create_subscription(
-            PoseArray,
-            '/vrx/wayfinding/waypoints',
+            PoseStamped,
+            '/vrx/stationkeeping/goal',
             self.listener_callback,
             10)
         self.publisher = self.create_publisher(Gateway, '/send_to_gateway', 10)
 
-    def listener_callback(self, msg):
+    def listener_callback(self, msg: PoseStamped):
         waypt_msg = Gateway()
-        inner_string = ""
-        for i, pose in enumerate(msg.poses):
-            inner_string += f"{pose.position.x},{pose.position.y}"
-            if i < len(msg.poses) - 1:
-                inner_string += ":"
         waypt_msg.gateway_key = "WPT_UPDATE_GPS"
-        waypt_msg.gateway_string = inner_string
+        waypt_msg.gateway_string = f"{msg.pose.position.x},{msg.pose.position.y}"
         self.get_logger().info('Publishing: "%s"' % waypt_msg.gateway_string)
         self.publisher.publish(waypt_msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    pub_sub = WaypointPubSub()
-    rclpy.spin(pub_sub)
-    pub_sub.destroy_node()
+    node = StationKeeping()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
