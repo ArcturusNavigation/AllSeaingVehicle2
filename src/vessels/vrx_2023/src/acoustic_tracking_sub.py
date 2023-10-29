166504302e370c095992b1c9debd95dd5473ac7c
Toya Takahashi <toyatakahashi522@gmail.com>
2023-10-29 17:59:08 -0400

    half fixed acoustic tracking

--- src/vessels/vrx_2023/src/acoustic_tracking_sub.py
+++ src/vessels/vrx_2023/src/acoustic_tracking_sub.py
@@ -1,13 +1,11 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 
-from ros_gz_interfaces.msg  import ParamVec
+from ros_gz_interfaces.msg import ParamVec
 from nav_msgs.msg import Odometry 
 
-
-
-class MinimalSubscriber(Node):
-
+class AcousticTrackingSub(Node):
     def __init__(self):
         super().__init__('acoustic_tracking_sub')
         self.pinger_subscription = self.create_subscription(
@@ -17,30 +15,24 @@ class MinimalSubscriber(Node):
             10)
         self.odometry_subscription = self.create_subscription(
             Odometry,
-            'odometry/filtered',
+            '/odometry/filtered',
             self.odom_callback,
             10)
-        self.subscription  # prevent unused variable warning
 
     def pinger_callback(self, msg):
         self.get_logger().info('Pinger heard: "%s"' % msg.params[0])
+        pass
+
     def odom_callback(self, msg):
         self.get_logger().info('Odom heard x position: "%s"' % msg.pose.pose.position.x)
-
+        pass
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimal_subscriber = MinimalSubscriber()
-
-    rclpy.spin(minimal_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    node = AcousticTrackingSub()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
