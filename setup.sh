1d411f8826b99a48e79f71dc5b1910ffb5f28a11
Toya Takahashi <toyatakahashi522@gmail.com>
2023-11-02 00:31:08 -0400

    fix perception suite error

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt upgrade
 # MOOS-IvP requirements
 sudo apt-get install -y g++ cmake xterm
 sudo apt-get install -y libfltk1.3-dev freeglut3-dev libpng-dev libjpeg-dev 
-sudo apt-get install -y libxft-dev libxinerama-dev libtiff5-dev
+sudo apt-get install -y libxft-dev libxinerama-dev libtiff5-dev libprotobuf-dev
 
 # Basic ROS package requirements
 sudo apt install -y protobuf-compiler libb64-dev ros-humble-diagnostic-updater lsb-release wget gnupg 
