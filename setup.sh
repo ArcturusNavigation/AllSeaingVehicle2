66aa21f1722d7ef4b9a36423aa9fb92e4452cc17
Toya Takahashi <toyatakahashi522@gmail.com>
2023-11-01 23:22:07 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,18 @@
 sudo apt update && sudo apt upgrade
 
 # MOOS-IvP requirements
-sudo apt-get install g++ cmake xterm
-sudo apt-get install libfltk1.3-dev freeglut3-dev libpng-dev libjpeg-dev 
-sudo apt-get install libxft-dev libxinerama-dev libtiff5-dev
+sudo apt-get install -y g++ cmake xterm
+sudo apt-get install -y libfltk1.3-dev freeglut3-dev libpng-dev libjpeg-dev 
+sudo apt-get install -y libxft-dev libxinerama-dev libtiff5-dev
 
 # Basic ROS package requirements
-sudo apt install protobuf-compiler libb64-dev ros-humble-diagnostic-updater lsb-release wget gnupg 
+sudo apt install -y protobuf-compiler libb64-dev ros-humble-diagnostic-updater lsb-release wget gnupg 
 
 # Install Gazebo Garden
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt update
-sudo apt install gz-garden python3-sdformat13 ros-humble-ros-gzgarden ros-humble-xacro
+sudo apt install -y gz-garden python3-sdformat13 ros-humble-ros-gzgarden ros-humble-xacro
 
 # Initialize submodule
 git submodule update --init --recursive src/robot_localization src/vrx
