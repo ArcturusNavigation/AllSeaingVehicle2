561c0857612ecf871d6f0a75c0d442d7ea9b6db5
the0 <the0@mit.edu>
2023-11-21 20:28:07 -0500

    names

--- src/vessels/vrx_2023/src/xdrive_controller.py
+++ src/vessels/vrx_2023/src/xdrive_controller.py
@@ -5,7 +5,7 @@ import math
 from rclpy.node import Node
 
 from std_msgs.msg import Float64MultiArray
-from std_msgs.msg import Float64
+from std_msgs.msg import Int64
 from asv_interfaces.msg import ASVState
 
 class Controller(Node):
@@ -14,7 +14,7 @@ class Controller(Node):
         l = 3.5
         w = 2
         self.max_output = 1000
-        self.max_input = 5
+        self.max_input = 1.1
         self.thrust_factor = self.max_output / self.max_input
         self.velocities = [0] * 3
         self.target_heading = None
@@ -23,11 +23,16 @@ class Controller(Node):
         self.previous_rotation = 0
 
         self.r = ((l ** 2 + w ** 2) / 2 - l * w) ** 0.5 / 2
+
+        self.front_left_name = "frontleft_pwm"
+        self.front_right_name = "frontright_pwm"
+        self.back_left_name = "backleft_pwm"
+        self.back_right_name = "backright_pwm"
         self.all_thruster_names = (
-            "front_left",
-            "front_right",
-            "back_left",
-            "back_right",
+            self.front_left_name,
+            self.front_right_name,
+            self.back_left_name,
+            self.back_right_name
         )
 
         self.create_subscription(
@@ -39,13 +44,13 @@ class Controller(Node):
         self.thrust_publishers = {}
         for thruster_prefix in self.all_thruster_names:
             self.thrust_publishers[thruster_prefix] = self.create_publisher(
-                Float64, f"/wamv/thrusters/{thruster_prefix}/thrust", 10
+                Int64, thruster_prefix, 10
             )
 
     def update_heading(self, msg):
         heading = -msg.nav_heading * math.pi/180
         if self.target_heading is None:
-            print(f"got initial heading = {heading}")
+            # print(f"got initial heading = {heading}")
             self.target_heading = heading # first time initialization
         tx, ty, tn = self.velocities
         d = 2 ** (3 / 2)
@@ -58,19 +63,19 @@ class Controller(Node):
             theta_diff = (self.target_heading - heading) % (2 * math.pi)
             if theta_diff > math.pi:
                 theta_diff -= 2 * math.pi
-            print(f"theta error: {theta_diff:3.3f}, theta = {heading:3.3f}, desired = {self.target_heading:3.3f}")
+            # print(f"theta error: {theta_diff:3.3f}, theta = {heading:3.3f}, desired = {self.target_heading:3.3f}")
         self.previous_rotation = tn
         tn += self.theta_kp * theta_diff
         tx, ty, tn = (self.restrict_input(inp) for inp in (tx, ty, tn))
         results = {
-            "back_right": (tx - ty) / d + tn / (4 * self.r),
-            "back_left": (ty + tx) / d - tn / (4 * self.r),
-            "front_left": (tx - ty) / d - tn / (4 * self.r),
-            "front_right": (ty + tx) / d + tn / (4 * self.r),
+            self.back_right_name: (tx - ty) / d + tn / (4 * self.r),
+            self.back_left_name: (ty + tx) / d - tn / (4 * self.r),
+            self.front_left_name: (tx - ty) / d - tn / (4 * self.r),
+            self.front_right_name: (ty + tx) / d + tn / (4 * self.r),
         }
         for name in self.all_thruster_names:
-            float_msg = Float64()
-            float_msg.data = results[name] * self.thrust_factor
+            float_msg = Int64()
+            float_msg.data = int(results[name] * self.thrust_factor)
             self.thrust_publishers[name].publish(float_msg)
 
     def restrict_input(self, input):
